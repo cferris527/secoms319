2a74f6f81e68ec2f6c5cb5840e33056028eda634
josephTJennings <josephjenningsct@gmail.com>
2023-10-17 15:37:03 -0500

    Corrected JSON and added sample javascript

--- midterm_project/artists.js
+++ midterm_project/artists.js
@@ -6,23 +6,24 @@ function setArtistImage(artistName) {
 
   // Fetch the JSON file
   fetch(jsonFilePath)
-    .then(response => response.json())
-    .then(data => {
-      if (data.Artists.hasOwnProperty(artistName)) {
-        const artistData = data.Artists[artistName];
-        const imgElement = document.getElementById("artistImage"); // Replace "artistImage" with your actual img tag's id
-        if (imgElement) {
-          imgElement.src = artistData.Image;
-        } else {
-          console.error("Image element not found in the HTML.");
-        }
+  .then(response => response.json())
+  .then(data => {
+    const artistData = data.Artists.find(artist => artist.hasOwnProperty(artistName));
+    if (artistData) {
+      const artistInfo = artistData[artistName];
+      const imgElement = document.getElementById("artistImage"); // Replace "artistImage" with your actual img tag's id
+      if (imgElement) {
+        imgElement.src = artistInfo.Image;
       } else {
-        console.error("Artist not found in the JSON data.");
+        console.error("Image element not found in the HTML.");
       }
-    })
-    .catch(error => {
-      console.error("Error loading the JSON file:", error);
-    });
+    } else {
+      console.error("Artist not found in the JSON data.");
+    }
+  })
+  .catch(error => {
+    console.error("Error loading the JSON file:", error);
+  });
 }
 
 // Usage example
