76fc610702df049e0927824fd431143f98454d3c
JosephTJennings <josephjenningsct@gmail.com>
2023-10-17 20:25:34 -0500

    Finished JSON and JS for Billie

--- midterm_project/artists.js
+++ midterm_project/artists.js
@@ -1,6 +1,6 @@
 console.log("TEST")
 
-function setArtistImage(artistName) {
+function setArtist(artistName, position) {
   // Replace with the actual path to your JSON file
   const jsonFilePath = 'data.json';
   const imageFolderPath = '/images/artists/'; 
@@ -12,12 +12,22 @@ function setArtistImage(artistName) {
     const artistData = data.Artists.find(artist => artist.hasOwnProperty(artistName));
     if (artistData) {
       const artistInfo = artistData[artistName];
-      const imgElement = document.getElementById("artistImage"); // Replace "artistImage" with your actual img tag's id
-      if (imgElement) {
-        imgElement.src = `${imageFolderPath}${artistName.toLowerCase()}.jpeg`;
-      } else {
-        console.error("Image element not found in the HTML.");
-      }
+      const imgElement = document.getElementById("artistImage" + position.toString()); // Replace "artistImage" with your actual img tag's id
+      const pBorn = document.getElementById("born" + position.toString());
+      const pHome = document.getElementById("hometown" + position.toString());
+      const pFunFact = document.getElementById("fun-fact" + position.toString());
+      const pAlbumName = document.getElementById("album-name" + position.toString());
+      const pSong1 = document.getElementById("song" + position.toString() + "1");
+      const pSong2 = document.getElementById("song" + position.toString() + "2");
+
+      // set each HTML = the JSON DATA
+      imgElement.src = artistInfo.Image;
+      pBorn.innerHTML = pBorn.innerHTML + "&nbsp;" + artistInfo.Born;
+      pHome.innerHTML = pHome.innerHTML + "&nbsp;" + artistInfo.Hometown;
+      pFunFact.innerHTML = pFunFact.innerHTML + "&nbsp;" + artistInfo.FunFact;
+      pAlbumName.innerHTML = pAlbumName.innerHTML + "&nbsp;" + artistInfo.Album;
+      pSong1.innerHTML = artistInfo.Songs[0];
+      pSong2.innerHTML = artistInfo.Songs[1];
     } else {
       console.error("Artist not found in the JSON data.");
     }
@@ -28,9 +38,9 @@ function setArtistImage(artistName) {
 }
 
 function setAllArtists(){
-  const artists = ["Drake", "Billie"]
+  const artists = ["Billie", "Drake"]
   for(let i = 0; i < artists.length; i++) {
-    setArtistImage(artists[i])
+    setArtist(artists[i], i)
   }
 }
 
