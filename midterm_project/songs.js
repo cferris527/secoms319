225976dbacffd5330a62b90507972bc1496b9e1b
cferris <connor27@iastate.edu>
2023-10-19 20:53:25 -0500

    Songs and About pages complete

--- midterm_project/songs.js
+++ midterm_project/songs.js
@@ -1,41 +1,29 @@
-// Function to create divs for albums
-function createAlbumDivs(data) {
-    const albumList = document.getElementById("AlbumList");
-  
-    data.forEach((album) => {
-      const albumDiv = document.createElement("div");
-      albumDiv.className = "album";
-  
-      // Create HTML structure for the album
-      console.log(album.Image)
-      albumDiv.innerHTML = `
-        <h2>${album.Name}</h2>
-        <img src = "${album.Image}">
-        <p>Singer: ${album.Singer}</p>
-        <p>Date: ${album.Date}</p>
-        <p>Top Songs:</p>
-        `;
-    
-        // Create an unordered list for songs
-        const songList = document.createElement("ul");
-    
-        album.Songs.forEach((song) => {
-          const songItem = document.createElement("li");
-          songItem.textContent = song;
-          songList.appendChild(songItem);
-        });
-    
-        albumDiv.appendChild(songList);
-    
-        // Append the album div to the AlbumList div
-        albumList.appendChild(albumDiv);
+function createSongDivs(data) {
+    const songList = document.getElementById("SongList");
+    data.forEach((song) => {
+        const songDiv = document.createElement("div");
+        songDiv.className = "album";
+
+        // Create HTML structure for the song
+        console.log(song.Image)
+        songDiv.innerHTML = `
+          <h2>${song.Name}</h2>
+          <img src = "${song.Image}">
+          <p>Duration: ${song.Duration}</p>
+          <p>Plays: ${song.Plays}</p>
+          <p>Album: ${song.Album}</p>
+          <p>Artist: ${song.Artist}</p>
+          `;
+
+        // Append the song div to the songList div
+        songList.appendChild(songDiv);
     });
-  }
-  
-  // Read data from data.json
-  fetch('data.json')
+}
+
+// Read data from data.json
+fetch('data.json')
     .then((response) => response.json())
     .then((data) => {
-      createAlbumDivs(data.Albums);
+        createSongDivs(data.Songs);
     })
     .catch((error) => console.error('Error reading JSON data:', error));  
