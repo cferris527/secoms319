d4e968c4cc6986fd88a9712295f5124f02ea70b4
JosephTJennings <josephjenningsct@gmail.com>
2023-11-05 19:11:23 -0600

    Changed a few things

--- assignment02/team26_assignment2/src/App.js
+++ assignment02/team26_assignment2/src/App.js
@@ -1,50 +1,38 @@
-import React, { useState, useEffect} from 'react';
-import productsData from './products.json';
+import React, { useState } from 'react';
+import productsData from './products.json'; // Import the JSON data
 
 const App = () => {
   const [view, setView] = useState('browse');
-  const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
   const [search, setSearch] = useState('');
-  const [formData, setFormData] = useState({
-    fullName: '',
-    email: '',
-    card: '',
-    address: '',
-    // Add more fields as needed
-  });
 
-  useEffect(() => {
-    setProducts(productsData);
-  }, []);
+  const products = productsData;
 
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const handleAddToCart = (product) => {
+  const handleAddToCart = (productId) => {
     const updatedCart = { ...cart };
-    updatedCart[product.id] = (updatedCart[product.id] || 0) + 1;
+    updatedCart[productId] = (updatedCart[productId] || 0) + 1;
     setCart(updatedCart);
   };
 
   const handleRemoveFromCart = (productId) => {
-    const updatedCart = { ...cart };
-    if (updatedCart[productId] > 1) {
+    if (cart[productId] > 1) {
+      const updatedCart = { ...cart };
       updatedCart[productId] -= 1;
+      setCart(updatedCart);
     } else {
-      delete updatedCart[productId];
+      const { [productId]: _, ...updatedCart } = cart;
+      setCart(updatedCart);
     }
-    setCart(updatedCart);
   };
 
   const handleCheckout = () => {
+    // Implement the checkout logic here
     setView('cart');
   };
 
   const handleOrder = () => {
+    // Implement the order logic here
     setView('confirmation');
-    // Implement order handling and data submission here
   };
 
   const handleResetCart = () => {
@@ -52,23 +40,40 @@ const App = () => {
     setView('browse');
   };
 
-  const baseURL = "./images/"
+  const getTotal = () => {
+    let total = 0;
+    for (const productId in cart) {
+      const product = products.find((p) => p.id === parseInt(productId));
+      total += cart[productId] * product.price;
+    }
+    return total;
+  };
 
   return (
-    <div>
+    <div className="container">
+      <h1>Online Shopping</h1>
       {view === 'browse' && (
         <div>
-          <input type="text" placeholder="Search products" value={search} onChange={handleSearchChange} />
+          <h2>Browse View</h2>
+          <input
+            type="text"
+            placeholder="Search products"
+            onChange={(e) => setSearch(e.target.value)}
+          />
           <div className="row">
             {products
-              .filter((product) => product.name.toLowerCase().includes(search.toLowerCase()))
+              .filter((product) =>
+                product.name.toLowerCase().includes(search.toLowerCase())
+              )
               .map((product) => (
-                <div className="col" key={product.name}>
-                  <img src={require(baseURL + "apple.png")} alt={product.name} height = "100px" width = "120px"/>
+                <div className="col" key={product.id}>
+                  <img src={product.image} alt={product.name} />
                   <h3>{product.name}</h3>
                   <p>{product.description}</p>
                   <p>${product.price}</p>
-                  <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
+                  <button onClick={() => handleAddToCart(product.id)}>
+                    Add to Cart
+                  </button>
                 </div>
               ))}
           </div>
@@ -78,35 +83,38 @@ const App = () => {
 
       {view === 'cart' && (
         <div>
-          <h2>Cart</h2>
-          <div>
-            {Object.keys(cart).map((productId) => (
-              <div key={productId}>
-                <div>{products.find((product) => product.id === Number(productId)).name}</div>
-                <div>Quantity: {cart[productId]}</div>
-                <button onClick={() => handleRemoveFromCart(productId)}>Remove</button>
+          <h2>Cart View</h2>
+          {Object.keys(cart).map((productId) => (
+            <div key={productId}>
+              <div>
+                {products.find((product) => product.id === Number(productId)).name}
               </div>
-            ))}
-          </div>
-          <button onClick={handleOrder}>Checkout</button>
+              <div>Quantity: {cart[productId]}</div>
+              <button onClick={() => handleRemoveFromCart(productId)}>Remove</button>
+            </div>
+          ))}
+          <div>Total: ${getTotal().toFixed(2)}</div>
+          <button onClick={handleOrder}>Order</button>
+          <button onClick={handleResetCart}>Back to Browse</button>
         </div>
       )}
 
       {view === 'confirmation' && (
         <div>
-          <h2>Confirmation</h2>
-          <div>
-            {Object.keys(cart).map((productId) => (
-              <div key={productId}>
-                <div>{products.find((product) => product.id === Number(productId)).name}</div>
-                <div>Quantity: {cart[productId]}</div>
+          <h2>Confirmation View</h2>
+          {Object.keys(cart).map((productId) => (
+            <div key={productId}>
+              <img
+                src={products.find((product) => product.id === Number(productId)).image}
+                alt="Product"
+              />
+              <div>
+                {products.find((product) => product.id === Number(productId)).name}
               </div>
-            ))}
-          </div>
-          <div>Total: {/* Calculate total amount */}</div>
-          <form>
-            {/* Payment form fields and validation */}
-          </form>
+              <div>Quantity: {cart[productId]}</div>
+            </div>
+          ))}
+          <div>Total: ${getTotal().toFixed(2)}</div>
           <button onClick={handleResetCart}>Start a New Shopping Session</button>
         </div>
       )}
