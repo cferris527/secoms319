0d5bbb025b9f4ac8e52b76d093ded0b0e5fe2e81
JosephTJennings <josephjenningsct@gmail.com>
2023-12-03 12:49:16 -0600

    Added an express server to our react app.

---
+++ final_project/final-project-app/server/index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json());
+const port = "8081";
+const host = "localhost";
