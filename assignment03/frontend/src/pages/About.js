420544ea3f32374cdac040f9903ede97a29cd9c1
cferris <connor27@iastate.edu>
2023-12-10 16:57:41 -0600

    Moved code to navbar view / added about for assignment 3

--- assignment03/frontend/src/pages/About.js
+++ assignment03/frontend/src/pages/About.js
@@ -1,7 +1,35 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="infoList">
+      <h1>About the class</h1>
+      <h2>Date: December 10th, 2023</h2>
+      <p class="text1">
+        This website was created for COM S 319: Construction of User Interfaces.<br/>
+        This is "assignment 3", which covers creating a MERN application.<br/>
+        Mern stands for MongoDB, Express, React, Node.js. This application uses all of the following.<br/>
+        Core CRUD functions of Create, React, Update, Delete can all be used.
+      </p>
+      <h2>About the students</h2>
+      <div>
+        <h3>Connor Ferris</h3>
+        <p class="text1">
+          Connor is a junior studying computer science. He is from Honey Creek, IA.
+          You can reach him at connor27@iastate.edu
+        </p>
+      </div>
+      <div>
+        <h3>Joseph Jennings</h3>
+        <p class="text1">
+          Joseph is a senior in computer science. He is from St Louis, MO
+          Feel free to email him: jtj9@iastate.edu
+        </p>
+      </div>
+      <div>
+        <h3>Dr. Abraham N. Aldaco Gastelum</h3>
+        <p class="text1">
+          Dr. Aldaco is our professor who is overseeing this course. You can email him ataaldaco@iastate.edu
+        </p>
+      </div>
     </div>
   );
 };
--- assignment03/frontend/src/pages/Delete.js
+++ assignment03/frontend/src/pages/Delete.js
@@ -1,9 +1,98 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 
 const DeletePage = () => {
+
+  const [product, setProduct] = useState([]);
+  const [checked4, setChecked4] = useState(false);
+  const [viewer4, setViewer4] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const [viewer1, setViewer1] = useState(false);
+
+  function getOneByOneProductNext() {
+    if (product.length > 0) {
+      if (index === product.length - 1) setIndex(0);
+      else setIndex(index + 1);
+      if (product.length > 0) setViewer4(true);
+      else setViewer4(false);
+    }
+  }
+  function getOneByOneProductPrev() {
+    if (product.length > 0) {
+      if (index === 0) setIndex(product.length - 1);
+      else setIndex(index - 1);
+      if (product.length > 0) setViewer4(true);
+      else setViewer4(false);
+    }
+  }
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+  function getAllProducts() {
+    fetch("http://127.0.0.1:4000/api/get")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Show Catalog of Products :");
+        console.log(data);
+        setProduct(data);
+      });
+    setViewer1(!viewer1);
+  }
+
+  function deleteOneProduct(deleteid) {
+    console.log("Product to delete :", deleteid);
+    fetch(`http://127.0.0.1:4000/api/delete/${deleteid}`, { // Include deleteid in the URL
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to delete product");
+        }
+        return response.json(); // Parse JSON response
+      })
+      .then((data) => {
+        console.log("Delete a product completed : ", deleteid);
+        console.log(data);
+        // Assuming the server responds with { "message": "Post deleted successfully" }
+        alert(data.message); // Show the response message
+        getAllProducts(); // Call function to update products list
+      })
+      .catch((error) => {
+        console.error("Error deleting product:", error);
+        // Handle error here if needed
+      });
+  }
+
+  useEffect(() => {
+    getAllProducts();
+  }, [checked4]);
+
+
   return (
     <div>
-      <h1>Delete</h1>
+      <div>
+        <h3>Delete one product:</h3>
+        <input type="checkbox" id="acceptdelete" name="acceptdelete" checked={checked4}
+          onChange={(e) => setChecked4(!checked4)} />
+        <button onClick={() => getOneByOneProductPrev()}>Prev</button>
+        <button onClick={() => getOneByOneProductNext()}>Next</button>
+        <button onClick={() => deleteOneProduct(product[index].id)}>
+          Delete
+        </button>
+        {checked4 && (
+          <div key={product[index].id}>
+            <img src={product[index].image} width={30} /> <br />
+            Id:{product[index].id} <br />
+            Title: {product[index].title} <br />
+            Category: {product[index].category} <br />
+            Price: {product[index].price} <br />
+            Rating :{product[index].rating} <br />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
